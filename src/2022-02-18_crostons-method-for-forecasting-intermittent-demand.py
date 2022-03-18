7fbc79448948200c98bbd749da027d2ca3c3f4bd
Nayef Ahmad <nayef.ahmad.11@gmail.com>
2022-03-18 15:45:43 -0700

    Try several alpha values for clustered vs not clustered series

--- src/2022-02-18_crostons-method-for-forecasting-intermittent-demand.py
+++ src/2022-02-18_crostons-method-for-forecasting-intermittent-demand.py
@@ -138,7 +138,7 @@ fig.show()
 # ## Example 4
 
 # Here we try to see how forecasts change as we spread out/cluster the
-# positive values
+# positive values.
 
 y_not_clustered = pd.Series(
     [0, 1] + [0] * 3 + [1] + [0] * 3 + [1],
@@ -150,27 +150,27 @@ y_clustered = pd.Series(
     name="y_clustered",
 )
 
-smooth_value = 0.1  # todo: try different smooth_values
+smooth_values = [0.1, 0.5, 0.9]
 
-fig = plt.figure(figsize=(10, 5))
-for n, y_series in enumerate([y_not_clustered, y_clustered]):
-    forecaster = Croston(smoothing=smooth_value)
-    forecaster.fit(y_series)
-    y_pred = forecaster.predict(fh=[x for x in range(1, 4)])
+for smooth_value in smooth_values:
+    fig = plt.figure(figsize=(10, 5))
+    for n, y_series in enumerate([y_not_clustered, y_clustered]):
+        forecaster = Croston(smoothing=smooth_value)
+        forecaster.fit(y_series)
+        y_pred = forecaster.predict(fh=[x for x in range(1, 4)])
 
-    df_fitted = pd.DataFrame(
-        {"y": y_series, "y_fitted": forecaster._f[1:]}, index=y_series.index
-    )  # noqa
-    df_pred = pd.concat([df_fitted, y_pred])
-    df_pred = df_pred.rename(columns={0: "y_predicted"})
+        df_fitted = pd.DataFrame(
+            {"y": y_series, "y_fitted": forecaster._f[1:]}, index=y_series.index  # noqa
+        )
+        df_pred = pd.concat([df_fitted, y_pred])
+        df_pred = df_pred.rename(columns={0: "y_predicted"})
 
-    plt.suptitle("This is a suptitle", size=24, y=1.02)
-    ax = plt.subplot(2, 1, n + 1)
-    df_pred.plot(ax=ax)
-    ax.set_title(
-        f"Example 4 of forecast using Croston's method \nAlpha = {smooth_value}\n Series name = {y_series.name}"  # noqa
-    )
-    ax.set_ylim(0, 3)
-fig.tight_layout()
-fig.show()
-# fig.savefig('clustered-vs-not-clustered.pdf')
+        ax = plt.subplot(2, 1, n + 1)
+        df_pred.plot(ax=ax)
+        ax.set_title(
+            f"Example 4 of forecast using Croston's method \nAlpha = {smooth_value}\n Series name = {y_series.name}"  # noqa
+        )
+        ax.set_ylim(0, 3)
+    fig.tight_layout()
+    fig.show()
+    # fig.savefig(f'clustered-vs-not-clustered_smooth-{smooth_value}.pdf')
